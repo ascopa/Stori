ba3115c6f4b5dccb9fe5859deaf72a1bc1c5a724
Alejandro Scopa <ascopa@fi.uba.ar>
2025-07-17 00:01:27 -0300

    Deleting test files

--- process-user-transaction/internal/adapters/inbound/sqs/controller_test.go
+++ process-user-transaction/internal/adapters/inbound/sqs/controller_test.go
@@ -2,10 +2,12 @@ package sqs
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"io"
 	"os"
 	"process-user-transaction/internal/core/domain"
@@ -18,15 +20,61 @@ func TestHandleS3EventWithLocalCSV(t *testing.T) {
 	defer file.Close()
 
 	mockService := new(MockService)
-	mockService.On("ProcessUserTransactions", mock.Anything, mock.Anything, mock.Anything).Return(domain.UserTransactionInfo{Balance: decimal.NewFromFloat(15.55)}, nil)
 	mockS3Client := new(MockS3Client)
-	mockS3Client.On("GetObject", mock.Anything, mock.Anything, mock.Anything).Return(io.NopCloser(file), nil)
 	mockEBClient := new(MockEBClient)
+	mockS3Client.On("GetObject", mock.Anything, "stori-user-transactions", "transactions.csv").Return(io.NopCloser(file), nil)
+	mockService.On("ProcessUserTransactions", mock.Anything, mock.Anything, mock.Anything).Return(domain.UserTransactionInfo{}, nil)
 	mockEBClient.On("PutEvents", mock.Anything, mock.Anything).Return(nil)
 	controller := NewController(mockService, mockS3Client, mockEBClient)
 
-	err = controller.Handle(context.Background(), events.SQSEvent{})
+	err = controller.Handle(context.Background(), buildSQSEvent())
 	assert.NoError(t, err)
+	mockS3Client.AssertExpectations(t)
+	mockService.AssertExpectations(t)
+	mockEBClient.AssertExpectations(t)
+}
+
+func TestHandle_GetObjectError(t *testing.T) {
+	mockService := new(MockService)
+	mockS3Client := new(MockS3Client)
+	mockEBClient := new(MockEBClient)
+	mockS3Client.On("GetObject", mock.Anything, mock.Anything, mock.Anything).Return(io.NopCloser(nil), fmt.Errorf("failed to get S3 object"))
+	controller := NewController(mockService, mockS3Client, mockEBClient)
+
+	err := controller.Handle(context.Background(), buildSQSEvent())
+	require.ErrorContains(t, err, "failed to get S3 object")
+}
+
+func TestHandle_ProcessUserTransactionsError(t *testing.T) {
+	file, err := os.Open("trx.csv")
+	assert.NoError(t, err)
+	defer file.Close()
+
+	mockService := new(MockService)
+	mockS3Client := new(MockS3Client)
+	mockEBClient := new(MockEBClient)
+	mockS3Client.On("GetObject", mock.Anything, mock.Anything, mock.Anything).Return(io.NopCloser(file), nil)
+	mockService.On("ProcessUserTransactions", mock.Anything, mock.Anything, mock.Anything).Return(domain.UserTransactionInfo{}, fmt.Errorf("failed to process trx"))
+	controller := NewController(mockService, mockS3Client, mockEBClient)
+
+	err = controller.Handle(context.Background(), buildSQSEvent())
+	require.ErrorContains(t, err, "failed to process trx")
+}
+func TestHandle_PutEventsError(t *testing.T) {
+	file, err := os.Open("trx.csv")
+	assert.NoError(t, err)
+	defer file.Close()
+
+	mockService := new(MockService)
+	mockS3Client := new(MockS3Client)
+	mockEBClient := new(MockEBClient)
+	mockS3Client.On("GetObject", mock.Anything, mock.Anything, mock.Anything).Return(io.NopCloser(file), nil)
+	mockService.On("ProcessUserTransactions", mock.Anything, mock.Anything, mock.Anything).Return(domain.UserTransactionInfo{}, nil)
+	mockEBClient.On("PutEvents", mock.Anything, mock.Anything).Return(fmt.Errorf("failed to put event"))
+	controller := NewController(mockService, mockS3Client, mockEBClient)
+
+	err = controller.Handle(context.Background(), buildSQSEvent())
+	require.ErrorContains(t, err, "failed to put event")
 }
 
 type MockEBClient struct {
@@ -44,11 +92,7 @@ type MockS3Client struct {
 
 func (m *MockS3Client) GetObject(ctx context.Context, bucket string, key string) (io.ReadCloser, error) {
 	args := m.Called(ctx, bucket, key)
-	reader := args.Get(0)
-	if reader == nil {
-		return nil, args.Error(1)
-	}
-	return reader.(io.ReadCloser), args.Error(1)
+	return args.Get(0).(io.ReadCloser), args.Error(1)
 }
 
 type MockService struct {
@@ -58,4 +102,24 @@ type MockService struct {
 func (m *MockService) ProcessUserTransactions(transactions []domain.Transaction) (domain.UserTransactionInfo, error) {
 	args := m.Called(transactions)
 	return args.Get(0).(domain.UserTransactionInfo), args.Error(1)
+}
+
+func buildSQSEvent() events.SQSEvent {
+	bodyStruct := struct {
+		Bucket string `json:"bucket"`
+		Key    string `json:"key"`
+	}{
+		Bucket: "stori-user-transactions",
+		Key:    "transactions.csv",
+	}
+
+	bodyBytes, _ := json.Marshal(bodyStruct)
+
+	return events.SQSEvent{
+		Records: []events.SQSMessage{
+			{
+				Body: string(bodyBytes),
+			},
+		},
+	}
 }
